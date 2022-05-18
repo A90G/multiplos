d1fcaedf93f806a78f44fec21ca251152bc969e9
A90G <104040612+A90G@users.noreply.github.com>
2022-05-17 21:21:22 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+//Cree un método esMultiplo con 2 parámetros que devuelva el valor lógico verdadero o falso según si el primer número que
+//se indique como parámetro es múltiplo del segundo
+
+
+
+
+
+
+//Recuerde que un numero es múltiplo de otro si al dividirlo su resto es cero
+
+
+
+
+
+//Recuerde que la operación mod permite saber si el resto de una división es cero
