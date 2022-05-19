b005f0b14b77c6d3df066d5b6d6bd297b1f2bddf
A90G <104040612+A90G@users.noreply.github.com>
2022-05-19 19:28:56 -0300

    commit finalicé ejercicio

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 //Cree un método esMultiplo con 2 parámetros que devuelva el valor lógico verdadero o falso según si el primer número que
 //se indique como parámetro es múltiplo del segundo
+let num1: number = Number(prompt("ingrese un numero entero"));
+let num2: number = Number(prompt("ingrese un numero entero"));
+esMultiplo (num1, num2);
 
+if ( esMultiplo (num1, num2)){
+  console.log(num1 ,"es mùltiplo de", num2);
+} else {
+  console.log(num1 ,"NO es mùltiplo de", num2);
+}
 
-
-
-
+function esMultiplo( num1: number , num2: number): boolean {
+  return ((num1 % num2 )=== 0);}
 
 //Recuerde que un numero es múltiplo de otro si al dividirlo su resto es cero
-
-
-
-
-
 //Recuerde que la operación mod permite saber si el resto de una división es cero
+
